744231826e3db87dd6daf4cfc346e16d408a634e
Oleksiy Prokopan <alexeyprokopan@gmail.com>
2023-06-16 14:13:33 +0200

    Transfer from Codesandbox to VScode

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+/**
+ * Возвращает разницу двух чисел
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} Разница чисел
+ */
+function diff(a, b) {
+  return a - b;
+}
+
+const result = diff(10, 3);
+
+console.log(result);
